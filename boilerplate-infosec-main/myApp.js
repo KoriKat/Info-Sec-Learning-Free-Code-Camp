1d9b2b70230cb4e167108b86befbabb407352efc
KoriKat <166819199+KoriKat@users.noreply.github.com>
2024-04-12 21:19:54 +0000

    Add myapp.js

--- boilerplate-infosec-main/myApp.js
+++ boilerplate-infosec-main/myApp.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3030;
+
 const helmet = require('helmet');
+
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
+
 
 
 
